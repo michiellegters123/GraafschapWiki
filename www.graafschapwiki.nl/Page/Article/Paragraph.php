9d40aa20b60ebb08cdacbe630204aa7344a77285
GeestWagen <nkolpa12@gmail.com>
2019-02-08 14:17:21 +0100

    finished the basic article system

--- www.graafschapwiki.nl/Page/Article/Paragraph.php
+++ www.graafschapwiki.nl/Page/Article/Paragraph.php
@@ -10,14 +10,35 @@ class SubParagraph
         $this->title = $title;
         $this->text = $text;
     }
+
+    public function write()
+    {
+        echo "<p class='ContentText SubParagraphTitle'>$this->title</p>";
+        echo "<p class='ContentText'>$this->text</p>";
+    }
 }
 
 class Paragraph
 {
     private $subParagraphs = array();
+    private $title;
+
+    public function __construct($title)
+    {
+        $this->title = $title;
+    }
 
     public function addSub(SubParagraph $sub)
     {
         array_push($this->subParagraphs, $sub);
+    }
+
+    public function write()
+    {
+        echo "<h2 class='ParagraphTitle'>$this->title</h2>";
+        foreach ($this->subParagraphs as $item)
+        {
+            $item->write();
+        }
     }
 }
--- www.graafschapwiki.nl/Page/Article/index.php
+++ www.graafschapwiki.nl/Page/Article/index.php
@@ -1,20 +1,36 @@
 <?php
 
+require_once("Paragraph.php");
+
 $GLOBALS['title'] = "GraafschapWiki";
 $GLOBALS['resFolder'] = "../../res/";
 $GLOBALS['headerItems'] = array("<link rel=\"stylesheet\" href=\"../../res/css/content.css\">");
 include "../../include/views/Header.php";
 
+
+$test = new Paragraph("yeetest");
+$test->addSub(new SubParagraph("history", "fsdjfsldkajflkasdjfdsagay"));
+$test->addSub(new SubParagraph("boi", "sadfsadfaksdfklsdjflkasdjflkjads"));
+
 //data
 $ARTICLE_TITLE= "PHP";
 $ARTICLE_INDEX = "dfasdfasdfasdfasdfsdfdsfsa0";
-$ATRICLE_CONTENTS = array();
+$ATRICLE_CONTENTS = array($test);
 
 ?>
 
 
 <h1 class="Title"><?php echo $ARTICLE_TITLE; ?></h1>
 <p class="ContentText"><?php echo $ARTICLE_INDEX; ?></p>
+
+<?php
+
+foreach ($ATRICLE_CONTENTS as $paragraph)
+{
+    $paragraph->write();
+}
+
+?>
 
 <?php
 
