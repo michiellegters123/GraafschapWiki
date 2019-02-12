ca844784547e3366bebf5f02fa9bbce8d9fe8de0
GeestWagen <nkolpa12@gmail.com>
2019-02-12 11:46:22 +0100

    update dbo

--- sql/dbo.sql
+++ sql/dbo.sql
@@ -8,33 +8,33 @@ CREATE TABLE users
 	email VARCHAR(128) UNIQUE NOT NULL,
 	password VARCHAR(73) NOT NULL
 
-);
+) DEFAULT CHARSET=utf8;
 
 
 CREATE TABLE article
 (
   articleid INT(11) PRIMARY KEY AUTO_INCREMENT,
   title VARCHAR(512)  NOT NULL,
-  intro TEXT(1)
-);
+  intro TEXT
+)DEFAULT CHARSET=utf8;
 
 CREATE TABLE paragraph
 (
   paragraphid INT(11) PRIMARY KEY AUTO_INCREMENT,
   article INT(11),
   title varchar(521) NOT NULL
-);
+)DEFAULT CHARSET=utf8;
 
 CREATE TABLE subparagraph
 (
   subparagraphid INT(11) PRIMARY KEY AUTO_INCREMENT,
   paragraph INT(11),
-  content TEXT(1),
+  content TEXT,
   title varchar(521) NOT NULL
-);
+)DEFAULT CHARSET=utf8;
 
 CREATE TABLE paragraphimage
 (
   subparagraphid INT(11) PRIMARY KEY,
   filename varchar(128) NOT NULL
-);
+)DEFAULT CHARSET=utf8;
