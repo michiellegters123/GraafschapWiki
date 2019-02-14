ec3ff5cc16aa547e29e4cfee8c08baf0ca1e5663
GeestWagen <nkolpa12@gmail.com>
2019-02-14 14:55:32 +0100

    update edit

--- www.graafschapwiki.nl/res/js/Edit.js
+++ www.graafschapwiki.nl/res/js/Edit.js
@@ -4,14 +4,13 @@ function textOninput(thisElement)
     thisElement.style.height = thisElement.scrollHeight + "px";
 }
 
-window.onload = function()
+window.onload = function ()
 {
     var textAreas = document.getElementsByClassName("EditArea");
 
-    for(var i = 0; i < textAreas.length; i++)
+    for (var i = 0; i < textAreas.length; i++)
     {
         textAreas[i].setAttribute("oninput", "textOninput(this)");
         textOninput(textAreas[i]);
     }
-    console.log("yeet");
 };
